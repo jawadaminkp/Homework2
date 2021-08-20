fed19d203e2f6853e6ae8a004dfb07631383465f
Jawad Amin <59015763+jawadaminkp@users.noreply.github.com>
2021-08-20 12:27:48 +0500

    Add files via upload

---
+++ ims-clone/assets/app.js
@@ -0,0 +1,18 @@
+const links = document.querySelectorAll(".navbar-main .nav-item")
+const content = document.querySelectorAll(".collapse-content");
+
+
+
+
+
+for(let i = 0; i < links.length; i++ ) {
+    links[i].addEventListener("mouseover", hoverEnable);
+    links[i].addEventListener("mouseout", hoverDisable);
+
+    function hoverEnable() {
+        content[i].classList.add("active");
+    }
+    function hoverDisable() {
+        content[i].classList.remove("active");
+    }
+}
